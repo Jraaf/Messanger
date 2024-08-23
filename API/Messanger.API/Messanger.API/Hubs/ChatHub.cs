c82199ad3340db53898d14f25c58868fe3854858
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-08-23 17:15:21 +0300

    create hub

---
+++ API/Messanger.API/Messanger.API/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Messanger.API.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Messanger.API.Hubs;
+
+public class ChatHub:Hub
+{
+    public async Task JoinChat(UserConnection connection)
+    {
+        await Clients.All
+            .SendAsync("RecieveMessage", "admin",$"{connection.UserName} has joined the chat.");
+    }
+
+    public async Task JoinSpecificChat(UserConnection connection)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+        await Clients.Group(connection.ChatRoom).
+            SendAsync("RecieveMessage","admin",$"{connection.UserName} has joined the chat.");
+    }
+
+}
