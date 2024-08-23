dbb259350634240ec310a14beaa3bee0fa4510b0
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-08-23 23:28:48 +0300

    bug fix

--- API/Messanger.API/Messanger.API/Program.cs
+++ API/Messanger.API/Messanger.API/Program.cs
@@ -1,9 +1,13 @@
 using Messanger.API.Hubs;
+using Messanger.API.Models;
+using System.Net.Security;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSignalR();
+builder.Services.AddSingleton<IDictionary<string, UserConnection>>(
+    new Dictionary<string, UserConnection>());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
