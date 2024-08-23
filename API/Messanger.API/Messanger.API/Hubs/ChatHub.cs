84521fa9f473f90f565842b1269734759d427b79
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-08-23 22:30:22 +0300

    update hub

--- API/Messanger.API/Messanger.API/Hubs/ChatHub.cs
+++ API/Messanger.API/Messanger.API/Hubs/ChatHub.cs
@@ -3,19 +3,47 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Messanger.API.Hubs;
 
-public class ChatHub:Hub
+public class ChatHub(IDictionary<string, UserConnection> _connection) : Hub
 {
     public async Task JoinChat(UserConnection connection)
     {
         await Clients.All
-            .SendAsync("RecieveMessage", "admin",$"{connection.UserName} has joined the chat.");
+            .SendAsync("RecieveMessage", "admin", $"{connection.UserName} has joined the chat.");
     }
 
     public async Task JoinSpecificChat(UserConnection connection)
     {
-        await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
-        await Clients.Group(connection.ChatRoom).
-            SendAsync("RecieveMessage","admin",$"{connection.UserName} has joined the chat.");
+        await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom!);
+        await Clients.Group(connection.ChatRoom!).
+            SendAsync("RecieveMessage", "admin", $"{connection.UserName} has joined the chat.");
+        await SendConnectedUser(connection.ChatRoom!);
+    }
+    public async Task SendMessage(string message)
+    {
+        if (_connection.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+        {
+            await Clients.Group(userConnection.ChatRoom)
+                    .SendAsync("RecieveMessage", userConnection.UserName, message);
+        }
     }
 
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        if (!_connection.TryGetValue(Context.ConnectionId, out UserConnection connection))
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+        Clients.Group(connection.ChatRoom!)
+            .SendAsync("RecieveMessage", "admin", $"{connection.UserName} has left the chat.");
+        SendConnectedUser(connection.ChatRoom!);
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    private Task SendConnectedUser(string room)
+    {
+        var users = from v in _connection.Values
+                    where v.ChatRoom == room
+                    select v;
+        return Clients.Group(room).SendAsync("ConnectedUser", users);
+    }
 }
